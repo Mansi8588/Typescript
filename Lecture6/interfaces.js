41a0908d895154ac6aa6f57a3c7cec47a173890f
Mansi8588 <ajmrhw12@gmail.com>
2025-09-28 01:12:00 +0530

    Interface and type

--- Lecture6/interfaces.js
+++ Lecture6/interfaces.js
@@ -1,4 +1,46 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log("snbhdvg");
-//# sourceMappingURL=interfaces.js.map
+//! INterface
+var user;
+user = {
+    name: "younes",
+    age: 22,
+    greet: function (text) {
+        console.log(text + " " + this.name);
+    }
+};
+console.log(user);
+user.greet("hi there - i am");
+var Per = /** @class */ (function () {
+    function Per(n, age) {
+        this.name = n;
+        this.age = age;
+    }
+    Per.prototype.greet = function (text) {
+        console.log(text + " " + this.name);
+    };
+    return Per;
+}());
+var p1 = new Per("younes", 22);
+p1.greet("hello there - i am");
+var Person2 = /** @class */ (function () {
+    function Person2(n) {
+        this.name = n;
+    }
+    Person2.prototype.greed = function (text) {
+        console.log(text + " " + this.name);
+    };
+    return Person2;
+}());
+var p2 = new Person2("younes");
+p2.greed("hello there - i am");
+var Person3 = /** @class */ (function () {
+    function Person3(n) {
+        this.name = n;
+    }
+    Person3.prototype.greet = function (text) {
+        console.log(text + " " + this.name);
+    };
+    return Person3;
+}());
+var p3;
+p3 = new Person3("younes");
+console.log(p3);
--- Lecture6/interfaces.ts
+++ Lecture6/interfaces.ts
@@ -1,1 +1,132 @@
-console.log("snbhdvg")
+//! INterface
+
+
+// type Person ={
+//     name: string;
+//     age: number;
+//     greet(text: string): void;
+// }
+
+interface Person {
+    name: string;
+    age: number;
+    greet(text: string): void;
+}
+
+let user:Person
+
+user={
+    name: "younes",
+    age: 22,
+    greet(text: string){
+        console.log(text + " " + this.name)
+    
+    }
+}
+
+console.log(user) 
+user.greet("hi there - i am")
+
+
+class Per implements Person{
+    name: string;
+    age: number;
+    constructor(n:string,age:number){
+this.name=n;
+this.age=age;
+
+}
+greet(text: string): void {
+    console.log(text + " " + this.name)
+}
+}
+
+
+const p1=new Per("younes",22)
+p1.greet("hello there - i am")
+
+
+
+
+// !readonly properties
+// Access Modifiers not use here
+// readonly properties can only be initialized when they are declared or in the constructor of the class.
+
+interface Greed{
+   readonly name: string;
+    greed(text: string): void;
+}
+
+class Person2 implements Greed{
+    name: string;
+    constructor(n:string){
+        this.name=n;
+    }
+    greed(text: string): void {
+        console.log(text + " " + this.name)
+    }
+}
+
+const p2=new Person2("younes")
+p2.greed("hello there - i am")
+// p2.name="younes2"  // cannot reassign because readonly
+
+
+
+
+
+// !Extending interfaces
+
+interface Named{
+    readonly name: string;
+}
+
+interface Greet extends Named{
+    greet(text: string): void;
+}
+class Person3 implements Greet{
+    name: string;
+    constructor(n:string){
+        this.name=n;
+    }
+    greet(text: string): void {
+        console.log(text + " " + this.name)
+    }
+}
+let p3:Greet
+ p3=new Person3("younes")
+ console.log(p3)
+
+
+
+//  type func=(n1:number,n2:number)=>number
+
+//  let add:func;
+//  add=(n1:number,n2:number)=>{
+//     return n1+n2
+//     }
+
+//    let x= add(2,3)
+//    console.log(x)
+
+
+interface Func{
+    (n1:number,n2:number):number
+}
+
+
+// Optional (?)
+
+interface Optional{
+    readonly name:string;
+    outputName?:string;
+}
+
+
+// no error even outputname is not define
+class Person4 implements Optional{
+    name: string;
+    constructor(n:string){
+        this.name=n;
+   }
+}
